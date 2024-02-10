566bb4eb25332f31d41c3253d582ff5eaa6ff1a8
Tiago Oliveira <contato@otao.dev.br>
2024-02-10 20:23:40 -0300

    feat: create the factories

---
+++ database/factories/CompanyFactory.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Company>
+ */
+class CompanyFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'commission_rate' => fake('pt_BR')->numberBetween(1, 6),
+            'name' => fake('pt_BR')->company(),
+        ];
+    }
+}
